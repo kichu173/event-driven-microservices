b932a50a7307b9ffd45c0e2725bbaec2f51ee961
Kiran Kumar K <kichu173.kk@gmail.com>
2022-10-04 23:40:31 +0530

    event driven micro services with Kafka as message broker

---
+++ order-service/src/main/java/com/demo/orderservice/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.demo.orderservice.controller;
+
+import com.demo.basedomains.dto.Order;
+import com.demo.basedomains.dto.OrderEvent;
+import com.demo.orderservice.kafka.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    @Autowired
+    private OrderProducer orderProducer;
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order) {// added as dependency(base-domains) in pom.xml
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+        return "Order placed successfully!";
+    }
+}
---
+++ order-service/src/main/java/com/demo/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,38 @@
+package com.demo.orderservice.kafka;
+
+import com.demo.basedomains.dto.OrderEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class OrderProducer {
+
+    private NewTopic topic;
+
+    private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    @Autowired
+    public OrderProducer(NewTopic topic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.topic = topic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(OrderEvent event) {
+        log.info(String.format("Order event => %s", event.toString()));
+
+        // create Message
+        Message<OrderEvent> message = MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
